1de2492613594478dc092c4a58c9017dd4a7d63f
[leylairoyale] <[leylairoyale@gmail.com]>
2019-11-06 09:30:00 -0600

    Reformatted the book/author/titles to be stored in a dictionary instead of lists. Reformated the command line print option of the books.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from collections import Counter
+# from collections import Counter probably take that out.
 import operator
 
 key = 'AIzaSyCU2dyXMyOEp9rhcYonOmrhQ_ugikWyi9s'
@@ -16,21 +16,24 @@ class bookSearching:
         books = []
         authors = []
         publishers = []
+        bookinfo = {}
+        j = 1
         for i in range(5):
             try: 
-                books.append(data["items"][i]["volumeInfo"]["title"])
+                title = data["items"][i]["volumeInfo"]["title"]
             except:
-                books.append("No title listed")
+                title = "No title listed"
             try:
-                authors.append(data["items"][i]["volumeInfo"]["authors"])
+                author = data["items"][i]["volumeInfo"]["authors"]
             except:
-                authors.append("No authors listed")
+                author = "No authors listed"
             try: 
                 publisher = data["items"][i]["volumeInfo"]["publisher"]
-                publishers.append(publisher)
             except:
-                publishers.append("No publisher listed.")
-        bookinfo = list(zip(books, authors, publishers))
+                publisher = "No publisher listed."
+            bookinfo.update({j: [title, author, publisher]})
+            j += 1
+        # bookinfo = list(zip(books, authors, publishers))
         return bookinfo
 
 class userInput:
@@ -65,6 +68,11 @@ class userInput:
         keyword = input("What's the keyword for the book you want? ")
         return keyword
 
+    def does_user_want_to_select_or_search(self):
+        answer = input("Do you want to search again or select a book for your reading list?\
+            Type the book number to select a book or type 'search' to search again.")
+        return answer
+
     def select_a_book(self):
         selected_book = input("What book number do you want to select?")
         # if selected_book:
@@ -74,8 +82,8 @@ class userInputRules:
     def __init__(self):
         pass
 
-    def is_user_selection_a_number(self, selected_book):
-        return if type(selected_book) is int
+    # def is_user_selection_a_number(self, selected_book):
+    #     return if type(selected_book) is int
 
 class printToCL:
 
@@ -83,18 +91,24 @@ class printToCL:
         pass
 
     def print_book_info(self, bookinfo):
-        i = 1
+        # print(bookinfo[1][1])
+        j = 1
         for item in bookinfo:
-            print("Info on Book #"+ str(i) +": ")
-            print(item)
-            i = i + 1
+            print('Book #' + str(j) + ':')
+            print(bookinfo[j][0])
+            print('Author: ')
+            for auth in bookinfo[j][1]:
+                print(auth)
+            print('Publisher: ' + bookinfo[j][2])
+            j += 1
 
 class runProgram:
 
-    def __init__(self, bookSearching, userInput, printToCL):
+    def __init__(self, bookSearching, userInput, printToCL, userInputRules):
         self.bookSearching = bookSearching
         self.userInput = userInput
         self.printToCL = printToCL
+        self.userInputRules = userInputRules
 
     def make_the_program_work(self):
         text = self.userInput.get_search_text()
@@ -102,9 +116,11 @@ class runProgram:
         keyword = self.userInput.get_search_keyword()
         bookinfo = self.bookSearching.search_for_books(text, searchtype, keyword)
         self.printToCL.print_book_info(bookinfo)
+        self.userInput.does_user_want_to_select_or_search()
 
 booksearch = bookSearching()
 userinput = userInput()
 printtocl = printToCL()
-rundammit = runProgram(booksearch, userinput, printtocl)
+userinputrules = userInputRules()
+rundammit = runProgram(booksearch, userinput, printtocl, userinputrules)
 rundammit.make_the_program_work()
--- readme.txt
+++ readme.txt
@@ -6,10 +6,19 @@ in building this project:
 
 1. created a conda environment, google_books_env
 
-
 You left off trying to get the user to select a book. YOu'll have to re-do how 
 you're grabbing and organizing the book info cause it's kinda confusing.
 But, you'll want to add that and have the user select a book by number.
 Then, that number can be added to the bookshelf called ReadingList.
 The user will then have to be able to view the ReadingList
 From there, add in yer tests!
+
+You need to figure out how to better organize the info, so that the info
+for each book is listed separately on a separate line. I'm not sure how to do
+that, however. 
+Then, you need to be sure the person can grab a book and put in on a 
+reading list.
+
+Edge cases to solve for:
+if the user enters a keyword that doesn't work
+if the user wants to quit
