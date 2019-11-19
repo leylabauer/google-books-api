72d34a2bff74b353b066e3806f011f73c2d8e0a6
[leylairoyale] <[leylairoyale@gmail.com]>
2019-11-19 11:25:59 -0600

    Updated the readme.

--- test_main.py
+++ test_main.py
@@ -1,48 +1,49 @@
 from main import *
 
-booksearch = bookSearching()
-userinput = userInput()
-printtocl = printToCL()
-userinputrules = userInputRules()
-bookinfo = booksearch.bookinfo
-reading_list = booksearch.reading_list
+booksearching = BookSearching()
+readingList = ReadingList()
+userinput = UserInput()
+print_ = Print()
+rules = Rules()
+bookinfo = booksearching.bookinfo
+reading_list = booksearching.reading_list
 
 # To run the tests, the objects in main.py cannot be instantiated.
 # Comment them out to properly run the tests.
 
-def test_if_selected_book_is_int_is_true():
-    assert userinputrules.check_if_selected_book_is_int(2) is True
+def test_is_selected_book_integer_is_true():
+    assert Rules.is_selected_book_integer(2) is True
 
 def test_if_selected_book_is_int_is_false():
-    assert userinputrules.check_if_selected_book_is_int("text") is False
+    assert Rules.check_if_selected_book_is_int("text") is False
 
 def test_if_selected_book_is_in_list_is_true():
-    assert userinputrules.check_if_selected_book_int_is_in_list(3) is True
+    assert Rules.check_if_selected_book_int_is_in_list(3) is True
 
 def test_if_selected_book_is_in_list_is_false():
-    assert userinputrules.check_if_selected_book_int_is_in_list(6) is False
+    assert Rules.check_if_selected_book_int_is_in_list(6) is False
 
 def test_if_search_is_empty():
     bookinfo.update({1: ["Cats", "Desmond Bauer", "Meowing House Publishing"]})
-    assert userinputrules.check_if_search_is_empty(bookinfo) is False
+    assert Rules.check_if_search_is_empty(bookinfo) is False
 
 def test_if_search_is_empty():
     bookinfo.update({1: ["No title listed", "No authors listed", "No publisher listed"]})
-    assert userinputrules.check_if_search_is_empty(bookinfo) is True
+    assert Rules.check_if_search_is_empty(bookinfo) is True
 
 def test_if_searchtype_input_is_accurate_returns_true():
-    assert userinputrules.check_if_searchtype_input_is_accurate('intitle') is True
+    assert Rules.check_if_searchtype_input_is_accurate('intitle') is True
 
 def test_if_searchtype_input_is_accurate_returns_false():
-    assert userinputrules.check_if_searchtype_input_is_accurate('text') is False
+    assert Rules.check_if_searchtype_input_is_accurate('text') is False
 
 def test_if_reading_list_is_empty_returns_true():
-    assert userinputrules.check_if_reading_list_is_empty(reading_list) is True
+    assert Rules.check_if_reading_list_is_empty(reading_list) is True
 
 def test_add_selected_book_to_reading_list_works():
     bookinfo.update({1: ["Cats", "Desmond Bauer", "Meowing House Publishing"]})
-    booksearch.add_selected_book_to_reading_list(1, bookinfo)
+    booksearching.add_selected_book_to_reading_list(1, bookinfo)
     assert len(reading_list) is 1
 
 def test_if_reading_list_is_empty_returns_false():
-    assert userinputrules.check_if_reading_list_is_empty(reading_list) is False
+    assert Rules.check_if_reading_list_is_empty(reading_list) is False
